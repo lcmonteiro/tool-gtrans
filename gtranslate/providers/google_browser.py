c0fee4dea01b8c6451e861017995dc7082c37647
Luis Monteiro <luiscarlos.monteiro@altran.com>
2020-01-08 15:18:46 +0000

    update: process

--- gtranslate/providers/google_browser.py
+++ gtranslate/providers/google_browser.py
@@ -14,8 +14,11 @@ from time     import sleep, time
 # GoogleAPI 
 # -------------------------------------------------------------------------------------------------
 class GoogleBrowser:
-    __ENDL = '<+++>'
-    __ENDS = '<###>'
+    # TAGS
+    __ENDL  = "<+++>"
+    __ENDS  = "<###>"
+    __IAREA = "source"
+    __OAREA = "//span[contains(@class, 'tlid-translation')]"
 
     def __init__(self, to_lang='en', from_lang=None):
         self.__from   = from_lang if from_lang is not None else 'auto'
@@ -34,17 +37,25 @@ class GoogleBrowser:
             sleep(1)
             translated = self.__unserialize(self.__read())
             if len(translated) == len(data):
+                self.__clear()
                 return translated
+        self.__clear()
         raise TimeoutError()
     # -----------------------------------------------------------------------------------
     # private 
     # -----------------------------------------------------------------------------------
     def __engine(self, from_lang, to_lang):
+        from os.path import dirname, join, realpath
+        from random  import choice
+        # load user agents
+        with open(join(dirname(realpath(__file__)), 'uagents.txt')) as f:
+            uagents = [ua.strip() for ua in f if ua.strip()]
         # driver options
         options = webdriver.chrome.options.Options()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-notifications")
+        options.add_argument(f"user-agent={choice(uagents)}")
         # open driver
         driver = webdriver.Chrome(options=options)
         #driver = webdriver.Chrome()
@@ -52,20 +63,20 @@ class GoogleBrowser:
         return driver
 
     def __write(self, data):
-        # tags
-        IAREA = "source"
         # input area
-        iarea = self.__engine.find_element_by_id(IAREA)
-        iarea.clear()
-        sleep(2)
+        iarea = self.__engine.find_element_by_id(self.__IAREA)
         iarea.send_keys(data)
 
     def __read(self):
-        # tags
-        OAREA = "//span[contains(@class, 'tlid-translation')]"
         # output area
-        oarea = self.__engine.find_element_by_xpath(OAREA)
-        return ' '.join([e.text for e in oarea.find_elements_by_xpath('./span')])
+        oarea = self.__engine.find_element_by_xpath(self.__OAREA)
+        text  = oarea.text.replace('\n','')
+        return text if text else ' '.join(
+                [e.text for e in oarea.find_elements_by_xpath('./span')]).replace('\n','')
+
+    def __clear(self):
+        self.__engine.find_element_by_id(self.__IAREA).clear()
+        sleep(2)
 
     def __serialize(self, data):
         return f'\n{self.__ENDS}\n'.join([d.replace('\n', f'{self.__ENDL}') for d in data])
